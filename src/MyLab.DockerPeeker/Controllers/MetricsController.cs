442d3459a999eb60dfcda15f7f389c14f15edbe1
Andrey O <ozzy.ext@gmail.com>
2021-06-21 17:14:40 +0300

    Remove needless

---
+++ src/MyLab.DockerPeeker/Controllers/MetricsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace MyLab.DockerPeeker.Controllers
+{
+    [ApiController]
+    [Route("metrics")]
+    public class MetricsController : ControllerBase
+    {
+        private readonly ILogger<MetricsController> _logger;
+
+        public MetricsController(ILogger<MetricsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Task<IActionResult> Get()
+        {
+            
+        }
+    }
+}
