11aac0e6b02587fca4dba14f020e763a253ace39
Andrey O <ozzy.ext@gmail.com>
2021-06-21 17:14:49 +0300

    Add console logs

--- src/MyLab.DockerPeeker/Startup.cs
+++ src/MyLab.DockerPeeker/Startup.cs
@@ -24,7 +24,7 @@ namespace MyLab.DockerPeeker
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddLogging(b => b.AddConsole());
             services.AddControllers();
         }
 
