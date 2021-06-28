6358f8d461e3e641bfd9390c1a21d67ffda1840c
Andrey O <ozzy.ext@gmail.com>
2021-06-28 16:40:58 +0300

    Suppoert another yet digital sizing

--- src/UnitTests/DockerStatItemBehavior.cs
+++ src/UnitTests/DockerStatItemBehavior.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using MyLab.DockerPeeker.Tools;
 using Xunit;
 using Xunit.Abstractions;
@@ -64,6 +67,15 @@ namespace UnitTests
         }
 
         [Theory]
+        [MemberData(nameof(GetRealMetrics))]
+        //[InlineData("45dbf20cb2edc93b7f4d9e35912d63f515ddd5f7a29e6e4c94f4dac7e592f9ce\tinfonot-get-avatar\t0.00%\t728KiB / 7.795GiB\t0.01%\t39.7MB / 0B\t218MB / 187MB")]
+        public void ShouldParseRealMetrics(string testInput)
+        {
+            //Act && Assert
+            DockerStatItem.Parse(testInput);
+        }
+
+        [Theory]
         [InlineData("")]
         [InlineData(null)]
         [InlineData(" ")]
@@ -73,6 +85,12 @@ namespace UnitTests
             var ex = Assert.Throws<ArgumentException>(() => DockerStatItem.Parse(input));
 
             _output.WriteLine(ex.Message);
+        }
+
+        public static IEnumerable<object[]> GetRealMetrics()
+        {
+            return File.ReadAllLines("real-metrics.txt")
+                .Select(s => new object[] {s});
         }
     }
 }
