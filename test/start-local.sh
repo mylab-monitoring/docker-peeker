cb2b4303ab9b8e3c1646d392090d6aed4fb1b25c
Andrey O <ozzy.ext@gmail.com>
2023-01-24 21:12:11 +0300

    update test script

--- test/start-local.sh
+++ test/start-local.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 sudo docker run --rm -d -l label_1=label_value1 --name docker-peeker-local -p "9010:80" -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v /var/run/docker.sock:/var/run/docker.sock:ro mylabtools/docker-peeker:local
-
-x-www-browser http://localhost:9010/metrics
+curl -v http://localhost:9010/metrics
